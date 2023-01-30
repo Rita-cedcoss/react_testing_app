7ff3602d622fc3f85a8577ee904662903d24129f
Rita-cedcoss <ritapal@cedcoss.com>
2023-01-30 19:04:16 +0530

    task is completd

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Conter from './TestingApp/Conter';
+
+function App() {
+  return (
+   <Conter/>
+  );
+}
+
+export default App;
---
+++ src/TestingApp/Counter.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { userInfo } from 'os';
+import Conter from './Conter';
+test("Render count 1",()=>{
+    render(<Conter/>)
+    let renderCount=screen.getByTestId("count")
+    expect(renderCount).toHaveTextContent('1');    
+})
+test("increment by 1",()=>{
+    render(<Conter/>)
+    let renderCount=screen.getByTestId("count");
+    let incrementby1=screen.getByRole('button',{name:"+"});
+    expect(renderCount).toHaveTextContent("1");
+    userEvent.click(incrementby1);
+    expect(renderCount).toHaveTextContent("2");
+    let decrementby1=screen.getByRole('button',{name:"-"});
+    expect(renderCount).toHaveTextContent("2")
+    userEvent.click(decrementby1)
+    expect(renderCount).toHaveTextContent("1")
+})
+test("should not decrement value 0",()=>{
+    render(<Conter/>)
+    let renderCount=screen.getByTestId("count");
+    let decrementby1=screen.getByRole('button',{name:"-"});
+    expect(renderCount).toHaveTextContent("1")
+    userEvent.click(decrementby1)
+    expect(renderCount).toHaveTextContent("1")
+})

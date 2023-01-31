98c90e69c67c7a8d1284f220e05ca3818bfc7923
Rita-cedcoss <ritapal@cedcoss.com>
2023-01-31 12:12:42 +0530

    testing app

--- src/TestingApp/Conter.tsx
+++ src/TestingApp/Conter.tsx
@@ -1,21 +1,20 @@
 import React, { useState } from "react";
 
 const Conter = () => {
-    const[count,setCount]=useState(1);
-    //function for increment 
-    const increment=()=>{
-        setCount(count+1);
-    } 
-    // function for decrement
-    const decrement=()=>{
-        if(count!==1)
-        {
-            setCount(count-1);
-        }
+  const [count, setCount] = useState(1);
+  //function for increment
+  const increment = () => {
+    setCount(count + 1);
+  };
+  // function for decrement
+  const decrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
     }
+  };
   return (
     <div className="col-6 m-auto text-center mt-5">
-        <h4>Counter App Testing</h4>
+      <h4>Counter App Testing</h4>
       <div
         className="btn-group border mt-2 m-auto "
         role="group"
@@ -24,7 +23,9 @@ const Conter = () => {
         <button type="button" className="btn btn-dark" onClick={increment}>
           +
         </button>
-        <span className="fs-4 ps-3 pe-3" data-testId="count">{count}</span>
+        <span className="fs-4 ps-3 pe-3" data-testId="count">
+          {count}
+        </span>
         <button type="button" className="btn btn-dark" onClick={decrement}>
           -
         </button>
--- src/TestingApp/Counter.test.tsx
+++ src/TestingApp/Counter.test.tsx
@@ -1,32 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { userInfo } from 'os';
-import Conter from './Conter';
-test("Render count 1",()=>{
-    render(<Conter/>)
-    let renderCount=screen.getByTestId("count")
-    expect(renderCount).toHaveTextContent('1');    
-})
-test("increment by 1 and decrement by 1",()=>{
-    render(<Conter/>)
-    let renderCount=screen.getByTestId("count");
-    // increment test case
-    let incrementby1=screen.getByRole('button',{name:"+"});
-    expect(renderCount).toHaveTextContent("1");
-    userEvent.click(incrementby1);
-    expect(renderCount).toHaveTextContent("2");
-    // decrement test case 
-    let decrementby1=screen.getByRole('button',{name:"-"});
-    expect(renderCount).toHaveTextContent("2");
-    userEvent.click(decrementby1)
-    expect(renderCount).toHaveTextContent("1")
-})
-
-test("should not decrement value 0",()=>{
-    render(<Conter/>)
-    let renderCount=screen.getByTestId("count");
-    let decrementby1=screen.getByRole('button',{name:"-"});
-    expect(renderCount).toHaveTextContent("1")
-    userEvent.click(decrementby1)
-    expect(renderCount).toHaveTextContent("1")
-})
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { userInfo } from "os";
+import Conter from "./Conter";
+test("should render a counter with value of 1", () => {
+  render(<Conter />);
+  let renderCount = screen.getByTestId("count");
+  expect(renderCount).toHaveTextContent("1");
+});
+test("should increase count when plus button is clicked and should decrease count when minus button is clicked", () => {
+  render(<Conter />);
+  let renderCount = screen.getByTestId("count");
+  // increment test case
+  let incrementby1 = screen.getByRole("button", { name: "+" });
+  expect(renderCount).toHaveTextContent("1");
+  userEvent.click(incrementby1);
+  expect(renderCount).toHaveTextContent("2");
+  // decrement test case
+  let decrementby1 = screen.getByRole("button", { name: "-" });
+  expect(renderCount).toHaveTextContent("2");
+  userEvent.click(decrementby1);
+  expect(renderCount).toHaveTextContent("1");
+});
+test("should not decrease to less than 1", () => {
+  render(<Conter />);
+  let renderCount = screen.getByTestId("count");
+  let decrementby1 = screen.getByRole("button", { name: "-" });
+  expect(renderCount).toHaveTextContent("1");
+  userEvent.click(decrementby1);
+  expect(renderCount).toHaveTextContent("1");
+});
